23c73d687bb565e2123eea0fc4b31ff7135d9913
Alan Stewart <alstew13@hotmail.com>
2018-01-27 11:06:19 +0100

    CloudsManager
    Delay between clouds spawning and limit to 10 clouds on screen.

--- Assets/Scripts/CloudManager.cs
+++ Assets/Scripts/CloudManager.cs
@@ -6,6 +6,9 @@ public class CloudManager : MonoBehaviour {
 
 	public GameObject cloudPrefab;
 
+	public float nextCloud = 0.0f; // this is if you wish to allow projectile from                      //the beginning of the game it self.   
+	public float cloudCoolDown = 2.0f; // if the cooldown is say 5 secs
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,14 +16,19 @@ public class CloudManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			
-			Vector3 mousePos = Input.mousePosition;
-			Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-			objectPos.z = 0.0f;   
-			GameObject cloudObject = Instantiate (cloudPrefab, objectPos, transform.rotation) as GameObject;
-			cloudObject.transform.parent = GameObject.Find ("Clouds").transform;
 
+		if (GameObject.FindGameObjectsWithTag ("Cloud").Length < 10) {
+
+
+			if (Input.GetMouseButtonDown (0) && Time.time > nextCloud) {
+				nextCloud = Time.time + cloudCoolDown;
+				Vector3 mousePos = Input.mousePosition;
+				Vector3 objectPos = Camera.main.ScreenToWorldPoint (mousePos);
+				objectPos.z = 0.0f;   
+				GameObject cloudObject = Instantiate (cloudPrefab, objectPos, transform.rotation) as GameObject;
+				cloudObject.transform.parent = GameObject.Find ("Clouds").transform;
+				nextCloud = Time.time + cloudCoolDown;
+			}
 		}
 	}
 }
