67146cf91f482d1a456c7580419fbd5dcea6972e
Alan Stewart <alstew13@hotmail.com>
2018-01-27 02:29:48 +0100

    Cloud Spawn and destory

---
+++ Assets/Scripts/CloudBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviour : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if (coll.gameObject.tag == "Wall") {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/CloudManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudManager : MonoBehaviour {
+
+	public GameObject cloudPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Vector3 mousePos = Input.mousePosition;
+			Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+			objectPos.z = 0.0f;   
+			GameObject cloudObject = Instantiate (cloudPrefab, objectPos, transform.rotation) as GameObject;
+			cloudObject.transform.parent = GameObject.Find ("Clouds").transform;
+
+		}
+	}
+}
