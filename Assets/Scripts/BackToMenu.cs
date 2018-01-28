d35aae1c478bac7310009dc27fc6a9ec1dfd57ec
Alan Stewart <alstew13@hotmail.com>
2018-01-28 14:32:09 +0100

    Revert "Revert "Add stuff""
    This reverts commit 9697bcd4710fd6f6a7ef5484f0a197c1f21ee835.

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// load the specified Unity level
+	public void loadLevel(string leveltoLoad)
+	{
+		Debug.Log ("sdafasdfasdf");
+		// load the specified level
+		SceneManager.LoadScene (leveltoLoad);
+
+	}
+
+}
---
+++ Assets/Scripts/HouseIllumination.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HouseIllumination : MonoBehaviour {
+
+
+	public Slider energySlider;
+	private SpriteRenderer spriteRenderer;
+	public Sprite normalHouse;
+	public Sprite darkHouse;
+	public Sprite explodingHouse;
+	public Sprite glowingHouse;
+	public Sprite totallyExplodingHouse;
+
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float percent = energySlider.value / energySlider.maxValue;
+		if (percent <= 0.45) {
+			spriteRenderer.sprite = darkHouse;
+		} else if (percent <= 0.61) {
+			spriteRenderer.sprite = normalHouse;
+		} else if (percent <= 0.7) {
+			spriteRenderer.sprite = glowingHouse;
+		} else if (percent <= 0.8) {
+			spriteRenderer.sprite = explodingHouse;
+		} else {
+			spriteRenderer.sprite = totallyExplodingHouse;
+		}
+
+	}
+}
